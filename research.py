51ddc8e5f918d21b78c8c966e8cc1ef67453f6f1
brescianim <97706677+brescianim@users.noreply.github.com>
2023-09-25 12:29:33 -0400

    Add files via upload

---
+++ research.py
@@ -0,0 +1,5 @@
+# Euclidean Distance Formula: √((x1 - x2)² + (y1 - y2)²)
+# https://towardsdatascience.com/creating-fractals-in-python-a502e5fc2094
+# https://www.geeksforgeeks.org/python-edge-detection-using-pillow/
+# Download Pillow: https://pillow.readthedocs.io/en/latest/installation.html
+# Filigree Letter/Calligraphy Styles: https://www.lettering-daily.com/calligraphy-styles/
