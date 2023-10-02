3a0cf8cdb9cfcc4af70c733a2c2d30d7256887ee
brescianim <97706677+brescianim@users.noreply.github.com>
2023-10-02 13:41:57 -0400

    Update treeEdge.py

--- treeEdge.py
+++ treeEdge.py
@@ -1,7 +1,7 @@
 import PIL as pillow
 from PIL import Image, ImageFilter
 image = Image.open(r"downloads/tree.jpeg")
-image.convert("L")
-image.filter(ImageFilter.FIND_EDGES)
+image = image.convert("L")
+image = image.filter(ImageFilter.FIND_EDGES)
 image.save(r"downloads/treeEdge.jpeg")
 exit()
