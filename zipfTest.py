25bd167729025334b007c674d7c50271f7f983dd
brescianim <97706677+brescianim@users.noreply.github.com>
2023-10-08 20:26:43 -0400

    Add files via upload

---
+++ zipfTest.py
@@ -0,0 +1,21 @@
+import PIL as pillow
+from PIL import Image
+import zipf.factories
+from zipf.factories import ZipfFromList
+
+image = Image.open(r"treeEdge.png")
+
+colors = []
+
+width, height = image.size
+
+for y in range(height):
+    for x in range(width):
+        pixel = image.getpixel((x, y))
+        colors.append(pixel)
+
+my_factory = ZipfFromList()
+
+my_zipf = my_factory.run(colors)
+
+print(my_zipf)
