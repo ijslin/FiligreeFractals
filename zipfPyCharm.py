6dd3186f868aed577962c9054d23cecd66261994
brescianim <97706677+brescianim@users.noreply.github.com>
2023-10-24 14:29:52 -0400

    Add files via upload

--- zipfPyCharm.py
+++ zipfPyCharm.py
@@ -21,21 +21,26 @@ def count_shades(image):
     return histogram
 
 
-def get_zipf_color(link):
+def get_image(link):
     image = Image.open(link)
+    image = image.convert("L")
+    image = image.filter(ImageFilter.FIND_EDGES)
+    return image
+
+
+def get_zipf_color(image):
     image = image.convert("L")
     image = image.filter(ImageFilter.FIND_EDGES)
 
     histogram = count_shades(image)
     counts = list(histogram.values())
+
     slope, r2, yint = byRank(counts)
 
     return slope, r2
 
 
-def get_zipf_gray_edge(link):
-    image = Image.open(link)
-
+def get_zipf_gray_edge(image):
     histogram = count_shades(image)
     counts = list(histogram.values())
 
@@ -52,8 +57,10 @@ def euclidean(x1, y1, x2, y2):
 
 
 def main():
-    a = get_zipf_gray_edge(r"treeEdge.jpeg")
-    b = get_zipf_gray_edge(r"landscapeEdge.jpeg")
+    image1 = get_image(r"images/filigrees/C1-2r-Opening V-cropped.jpeg")
+    image2 = get_image(r"images/filigrees/C1-2v & 4r-cropped.jpeg")
+    a = get_zipf_gray_edge(image1)
+    b = get_zipf_gray_edge(image2)
 
     print(a)
     print()
