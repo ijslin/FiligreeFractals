f31eba6cf4c47e2736a2070b7166a4608216bd33
brescianim <97706677+brescianim@users.noreply.github.com>
2023-11-28 00:08:12 -0500

    Add files via upload

--- zipfPyCharm.py
+++ zipfPyCharm.py
@@ -3,6 +3,7 @@
 from PIL import Image, ImageFilter
 from zipf import *
 from math import *
+import os
 
 
 def count_shades(image):
@@ -30,6 +31,19 @@ def get_image(link):
     image = image.filter(ImageFilter.FIND_EDGES)  # Edge detects the image
 
     return image  # Returns the image object
+
+
+def get_image_gray(link):
+    image = Image.open(link)
+    image = image.convert("L")
+
+    return image
+
+
+def get_image_color(link):
+    image = Image.open(link)
+
+    return image
 
 
 def get_quadrants(image):
@@ -119,6 +133,9 @@ def recursive(image, list, depth):
 
 
 def recursive_test():
+    print("Test of Recursive Algorithm with 4 Images:")
+    print()
+
     image = get_image(r"images/test_images/recursive_test.jpeg")
     result = []
     recursive(image, result, 1)
@@ -126,45 +143,196 @@ def recursive_test():
 
     image1 = get_image(r"images/test_images/billmanaris.jpeg")
     r1 = []
-    recursive(image1, r1, 1)
-    print("Arbitrary Image Zipf: ", r1[4])
+    recursive(image1, r1, 0)
+    print("Arbitrary Image Zipf: ", r1[0])
     print("Top Left Quadrant Zipf: ", result[0])
 
     image2 = get_image(r"images/test_images/white-noise-1400x825.jpeg")
     r2 = []
-    recursive(image2, r2, 1)
-    print("White Noise Zipf: ", r2[4])
+    recursive(image2, r2, 0)
+    print("White Noise Zipf: ", r2[0])
     print("Top Right Quadrant Zipf: ", result[1])
 
     image3 = get_image(r"images/test_images/plain-black-background-02fh7564l8qq4m6d.jpeg")
     r3 = []
-    recursive(image3, r3, 1)
-    print("Background with Blemish Zipf: ", r3[4])
+    recursive(image3, r3, 0)
+    print("Background with Blemish Zipf: ", r3[0])
     print("Bottom Left Quadrant Zipf: ", result[2])
 
     image4 = get_image(r"images/test_images/tree.jpeg")
     r4 = []
-    recursive(image4, r4, 1)
-    print("Tree Zipf: ", r4[4])
+    recursive(image4, r4, 0)
+    print("Tree Zipf: ", r4[0])
     print("Bottom Right Quadrant Zipf: ", result[3])
+    print()
+
+
+def get_links(author, number_of_images, list):
+    monaco = os.listdir("images/filigrees/Monaco/")
+    roselli = os.listdir("images/filigrees/Roselli")
+    gherarducci = os.listdir("images/filigrees/Gherarducci")
+    camaldolese = os.listdir("images/filigrees/Camaldolese")
+
+    monaco.remove(".DS_Store")
+    roselli.remove(".DS_Store")
+    gherarducci.remove(".DS_Store")
+    camaldolese.remove(".DS_Store")
+
+    if (author == "Monaco") | (author == "monaco"):
+        images = number_of_images
+        if images > len(monaco):
+            images = len(monaco)
+
+        slug = "images/filigrees/Monaco/"
+
+        for i in range(images):
+            link = slug + monaco[i]
+            list.append(link)
+
+    elif (author == "Roselli") | (author == "roselli"):
+        images = number_of_images
+        if images > len(roselli):
+            images = len(roselli)
+
+        slug = "images/filigrees/Roselli/"
+
+        for i in range(images):
+            link = slug + roselli[i]
+            list.append(link)
+
+    elif (author == "Gherarducci") | (author == "gherarducci"):
+        images = number_of_images
+        if images > len(gherarducci):
+            images = len(gherarducci)
+
+        slug = "images/filigrees/Gherarducci/"
+
+        for i in range(images):
+            link = slug + gherarducci[i]
+            list.append(link)
+
+    elif (author == "Camaldolese") | (author == "camaldolese"):
+        images = number_of_images
+        if images > len(camaldolese):
+            images = len(camaldolese)
+
+        slug = "images/filigrees/Camaldolese/"
+
+        for i in range(images):
+            link = slug + camaldolese[i]
+            list.append(link)
+
+
+def get_all_links(list):
+    corale1 = os.listdir("images/filigrees/Corale 1/")
+    corale3 = os.listdir("images/filigrees/Corale 3/")
+    corale5 = os.listdir("images/filigrees/Corale 5/")
+    corale6 = os.listdir("images/filigrees/Corale 6/")
+    corale7 = os.listdir("images/filigrees/Corale 7/")
+    corale8 = os.listdir("images/filigrees/Corale 8/")
+    corale9 = os.listdir("images/filigrees/Corale 9/")
+    corale11 = os.listdir("images/filigrees/Corale 11/")
+    corale12 = os.listdir("images/filigrees/Corale 12/")
+    corale13 = os.listdir("images/filigrees/Corale 13/")
+    corale14 = os.listdir("images/filigrees/Corale 14/")
+    corale15 = os.listdir("images/filigrees/Corale 15/")
+    corale16 = os.listdir("images/filigrees/Corale 16/")
+    corale17 = os.listdir("images/filigrees/Corale 17/")
+    corale18 = os.listdir("images/filigrees/Corale 18/")
+    corale19 = os.listdir("images/filigrees/Corale 19/")
+
+    corale1.remove(".DS_Store")
+    corale3.remove(".DS_Store")
+    corale5.remove(".DS_Store")
+    corale6.remove(".DS_Store")
+    corale7.remove(".DS_Store")
+    corale8.remove(".DS_Store")
+    corale9.remove(".DS_Store")
+    corale11.remove(".DS_Store")
+    corale12.remove(".DS_Store")
+    corale13.remove(".DS_Store")
+    corale14.remove(".DS_Store")
+    corale15.remove(".DS_Store")
+    corale16.remove(".DS_Store")
+    corale17.remove(".DS_Store")
+    corale18.remove(".DS_Store")
+    corale19.remove(".DS_Store")
+
+    for image in corale1:
+        slug = "images/filigrees/Corale 1/"
+        url = slug + image
+        list.append(url)
+    for image in corale3:
+        slug = "images/filigrees/Corale 3/"
+        url = slug + image
+        list.append(url)
+    for image in corale5:
+        slug = "images/filigrees/Corale 5/"
+        url = slug + image
+        list.append(url)
+    for image in corale6:
+        slug = "images/filigrees/Corale 6/"
+        url = slug + image
+        list.append(url)
+    for image in corale7:
+        slug = "images/filigrees/Corale 7/"
+        url = slug + image
+        list.append(url)
+    for image in corale8:
+        slug = "images/filigrees/Corale 8/"
+        url = slug + image
+        list.append(url)
+    for image in corale9:
+        slug = "images/filigrees/Corale 9/"
+        url = slug + image
+        list.append(url)
+    for image in corale11:
+        slug = "images/filigrees/Corale 11/"
+        url = slug + image
+        list.append(url)
+    for image in corale12:
+        slug = "images/filigrees/Corale 12/"
+        url = slug + image
+        list.append(url)
+    for image in corale13:
+        slug = "images/filigrees/Corale 13/"
+        url = slug + image
+        list.append(url)
+    for image in corale14:
+        slug = "images/filigrees/Corale 14/"
+        url = slug + image
+        list.append(url)
+    for image in corale15:
+        slug = "images/filigrees/Corale 15/"
+        url = slug + image
+        list.append(url)
+    for image in corale16:
+        slug = "images/filigrees/Corale 16/"
+        url = slug + image
+        list.append(url)
+    for image in corale17:
+        slug = "images/filigrees/Corale 17/"
+        url = slug + image
+        list.append(url)
+    for image in corale18:
+        slug = "images/filigrees/Corale 18/"
+        url = slug + image
+        list.append(url)
+    for image in corale19:
+        slug = "images/filigrees/Corale 19/"
+        url = slug + image
+        list.append(url)
 
 
 def same_author():
-    links = [r"images/filigrees/Monaco/C1-2v _ 4r-cropped.jpg",
-             r"images/filigrees/Monaco/C1-16r-S w mandorla-croppedA.jpg",
-             r"images/filigrees/Monaco/C1-16r-S w mandorla-croppedS.jpg",
-             r"images/filigrees/Monaco/C1-19v-20r-Magdalene-cropped.jpg",
-             r"images/filigrees/Monaco/C1-20v-21r-Magdanele hymn 2-cropped.jpg",
-             r"images/filigrees/Monaco/C1-20v-21r-Magdanele hymn 2-croppedE.jpg",
-             r"images/filigrees/Monaco/C1-21v-Mary Magdalene hymn 3-cropped.jpg",
-             r"images/filigrees/Monaco/C1-22v-E w filigree 2-cropped.jpg",
-             r"images/filigrees/Monaco/C1-23v-S-cropped.jpg",
-             r"images/filigrees/Monaco/C1-24v-Letter I-1-croppedR.jpg",
-             r"images/filigrees/Monaco/C1-24v-Letter I-1-croppedS.jpg",
-             r"images/filigrees/Monaco/C1-25v-Q-2-cropped.jpg",
-             r"images/filigrees/Monaco/C1-27v-S-2-cropped.jpg",
-             r"images/filigrees/Monaco/C1-28r-O completion-cropped.jpg"]
-    filigree1 = get_image(r"images/filigrees/Monaco/C1-2r-Opening V-cropped.jpg")
+    print("Test with Multiple Filigrees with the Same Known Author:")
+    print()
+
+    links = []
+    get_links("Monaco", 15, links)
+    filigree1 = get_image(links[0])
+    test_link = links[0]
+    links.pop(0)
 
     print("Author: Monaco")
     test_number = 1
@@ -174,7 +342,42 @@ def same_author():
 
         filigree2 = get_image(link)
 
-        result1 = [r"images/filigrees/Monaco/C1-2r-Opening V-cropped.jpg"]
+        result1 = [test_link]
+        result2 = [link]
+
+        recursive(filigree1, result1, 1)
+        recursive(filigree2, result2, 1)
+
+        print("Filigree 1: ", result1)
+        print("Filigree 2: ", result2)
+
+        print("Euclidean Distance: ", euclidean(result1, result2))
+        test_number += 1
+        print()
+    print()
+
+
+def different_authors():
+    print("Test with Multiple Filigrees with Different Known Authors:")
+    print()
+
+    links = []
+    get_links("Monaco", 8, links)
+    get_links("Camaldolese", 7, links)
+    filigree1 = get_image(links[0])
+    test_link = links[0]
+    links.pop(0)
+
+    print("Author 1: Monaco")
+    print("Author 2: Camaldolese")
+    test_number = 1
+
+    for link in links:
+        print("Test", test_number, ":")
+
+        filigree2 = get_image(link)
+
+        result1 = [test_link]
         result2 = [link]
 
         recursive(filigree1, result1, 1)
@@ -188,39 +391,115 @@ def same_author():
         print()
 
 
-def different_authors():
-    filigree1 = get_image(r"images/filigrees/Monaco/C1-2r-Opening V-cropped.jpg")
-    filigree2 = get_image(r"images/filigrees/Roselli/C3-1v-Resurrection-6-E-cropped.jpeg")
+def experiment_1(image_type):
+    links = []
+    get_links("Monaco", 15, links)
+    get_links("Camaldolese", 3, links)
 
-    print("Author of Filigree 1: Monaco")
-    print("Author of Filigree 2: Roselli")
+    if image_type == "edge":
+        print("Edge-Detected Images")
+        print()
 
-    result1 = []
-    result2 = []
+        for i in range(len(links)):
+            print("Image", i + 1)
+            print()
+            filigree1 = get_image(links[i])
 
-    recursive(filigree1, result1, 1)
-    recursive(filigree2, result2, 1)
+            for j in range(len(links)):
+                if j <= i:
+                    continue
+                else:
+                    result1 = [links[i]]
+                    recursive(filigree1, result1, 1)
 
-    print("Filigree 1: ", result1)
-    print("Filigree 2: ", result2)
+                    filigree2 = get_image(links[j])
+                    result2 = [links[j]]
+                    recursive(filigree2, result2, 1)
 
-    print("Euclidean Distance: ", euclidean(result1, result2))
+                    print("Filigree 1: ", result1)
+                    print("Filigree 2: ", result2)
+                    print("Euclidean Distance: ", euclidean(result1, result2))
+                    print()
+
+    elif image_type == "gray":
+        print("Gray-Scaled Images")
+        print()
+
+        for i in range(len(links)):
+            print("Image", i + 1)
+            print()
+            filigree1 = get_image_gray(links[i])
+
+            for j in range(len(links)):
+                if j <= i:
+                    continue
+                else:
+                    result1 = [links[i]]
+                    recursive(filigree1, result1, 1)
+
+                    filigree2 = get_image_gray(links[j])
+                    result2 = [links[j]]
+                    recursive(filigree2, result2, 1)
+
+                    print("Filigree 1: ", result1)
+                    print("Filigree 2: ", result2)
+                    print("Euclidean Distance: ", euclidean(result1, result2))
+                    print()
+
+    elif image_type == "color":
+        print("Colored Images")
+        print()
+
+        for i in range(len(links)):
+            print("Image", i + 1)
+            print()
+            filigree1 = get_image_color(links[i])
+
+            for j in range(len(links)):
+                if j <= i:
+                    continue
+                else:
+                    result1 = [links[i]]
+                    recursive(filigree1, result1, 1)
+
+                    filigree2 = get_image_color(links[j])
+                    result2 = [links[j]]
+                    recursive(filigree2, result2, 1)
+
+                    print("Filigree 1: ", result1)
+                    print("Filigree 2: ", result2)
+                    print("Euclidean Distance: ", euclidean(result1, result2))
+                    print()
+
+
+def all_images():
+    links = []
+    get_all_links(links)
+
+    for i in range(len(links)):
+        print("Image", i + 1)
+        print()
+        filigree1 = get_image(links[i])
+
+        for j in range(len(links)):
+            if j <= i:
+                continue
+            else:
+                result1 = [links[i]]
+                recursive(filigree1, result1, 1)
+
+                filigree2 = get_image_color(links[j])
+                result2 = [links[j]]
+                recursive(filigree2, result2, 1)
+
+                print("Filigree 1: ", result1)
+                print("Filigree 2: ", result2)
+                print("Euclidean Distance: ", euclidean(result1, result2))
+                print()
 
 
 def main():
-    # print("Test of Recursive Algorithm with 4 Images:")
-    # print()
-    # recursive_test()
-    # print()
-
-    print("Test with Multiple Filigrees with the Same Known Author:")
-    print()
-    same_author()
-    print()
-
-    # print("Test with 2 Filigrees with Different Known Authors:")
-    # print()
-    # different_authors()
+    all_images()
 
 
 main()
